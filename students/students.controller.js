b3d9cab726e76e7188efde52265939d7b2e0e2af
HansonJoel <joelhanson002@gmail.com>
2026-06-17 15:21:21 +0100

    updated all controllers with try/catch and next(error)

--- students/students.controller.js
+++ students/students.controller.js
@@ -1,65 +1,88 @@
 const studentService = require("./students.service");
 
 // CREATE STUDENT
-const createStudentController = async (req, res) => {
-  const response = await studentService.createStudent(req.body);
+const createStudentController = async (req, res, next) => {
+  try {
+    const response = await studentService.createStudent(req.body);
 
-  return res.status(response.code).json({
-    message: response.message,
-    data: response.data,
-  });
+    return res.status(response.code).json({
+      message: response.message,
+      data: response.data,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 // CREATE BULK STUDENTS
-const createBulkStudentsController = async (req, res) => {
-  const response = await studentService.createBulkStudents(req.body);
+const createBulkStudentsController = async (req, res, next) => {
+  try {
+    const response = await studentService.createBulkStudents(req.body);
 
-  return res.status(response.code).json({
-    message: response.message,
-    data: response.data,
-  });
+    return res.status(response.code).json({
+      message: response.message,
+      data: response.data,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 // UPDATE STUDENT
-const updateStudentController = async (req, res) => {
-  const { id } = req.params;
+const updateStudentController = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const response = await studentService.updateStudents(id, req.body);
 
-  const response = await studentService.updateStudents(id, req.body);
-
-  return res.status(response.code).json({
-    message: response.message,
-    data: response.data,
-  });
+    return res.status(response.code).json({
+      message: response.message,
+      data: response.data,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
-const deleteStudentController = async (req, res) => {
-  const { id } = req.params;
-  const response = await studentService.deleteStudent(id);
+const deleteStudentController = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const response = await studentService.deleteStudent(id);
 
-  return res.status(response.code).json({
-    message: response.message,
-    data: response.data,
-  });
+    return res.status(response.code).json({
+      message: response.message,
+      data: response.data,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
-const getStudentController = async (req, res) => {
-  const { id } = req.params;
-  const response = await studentService.getStudent(id);
+const getStudentController = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const response = await studentService.getStudent(id);
 
-  return res.status(response.code).json({
-    message: response.message,
-    data: response.data,
-  });
+    return res.status(response.code).json({
+      message: response.message,
+      data: response.data,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 // GET ALL STUDENTS
-const getAllStudentsController = async (req, res) => {
-  const response = await studentService.getAllStudents(req.query);
+const getAllStudentsController = async (req, res, next) => {
+  try {
+    const response = await studentService.getAllStudents(req.query);
 
-  return res.status(response.code).json({
-    message: response.message,
-    data: response.data,
-  });
+    return res.status(response.code).json({
+      message: response.message,
+      data: response.data,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
